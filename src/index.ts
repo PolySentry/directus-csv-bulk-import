80c444e5399df7401b5f939d25151217ba64b09d
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2022-01-17 12:42:11 +0100

    Update src/index.ts
    Co-authored-by: Mateusz Benecki <benecki.mateusz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { defineModule } from '@directus/extensions-sdk';
 import ModuleComponent from './module.vue';
 
 export default defineModule({
-	id: 'bulk_import',
+	id: 'bulk-import',
 	name: 'Bulk import',
 	icon: 'publish',
 	routes: [
