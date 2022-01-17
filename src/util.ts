f8de3191278a1e5bec69be34e5a520cd607c88b3
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2022-01-17 12:46:08 +0100

    Update src/util.ts
    Co-authored-by: Mateusz Benecki <benecki.mateusz@gmail.com>

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,6 @@ export const parseCollectionName = (name: String): String => {
     return capitalizeFirstLetter(name).replaceAll('_', ' ')
 }
 
-export const capitalizeFirstLetter = (string: String): String => {
+export const capitalize = (string: String): String => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
